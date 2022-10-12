28d0f124e9e6325fdb33aeb6d29a9e2d56a259be
Shah din sani <shah.din.sani@example.com>
2022-10-11 19:52:56 -0500

    first commit

---
+++ src/main/java/com/shahkaar/oidcgcp/OidcGcpApplication.java
@@ -0,0 +1,13 @@
+package com.shahkaar.oidcgcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OidcGcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shahkaar/oidcgcp/OidcGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahkaar.oidcgcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
